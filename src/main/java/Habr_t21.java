576c4fb9fb5026831418e1ac3022286191923e8f
alexleru <alexleru@yandex.ru>
2019-08-30 00:27:08 +0300

    fix

--- src/main/java/Habr_t21.java
+++ src/main/java/Habr_t21.java
@@ -1,5 +1,10 @@
 public class Habr_t21 {
     public static void main(String[] args) {
+        RandomWithWeight randomWithWeight =
+                new RandomWithWeight(new int[]{1, 2, 3}, new int[]{1, 2, 10});
+        for (int i = 0; i < 50; i++) {
+            System.out.println(randomWithWeight.randomWithWeight());
+        }
 
     }
 }
--- src/main/java/RandomWithWeight.java
+++ src/main/java/RandomWithWeight.java
@@ -2,13 +2,37 @@ public class RandomWithWeight {
     private int[] value;
     private int[] weight;
     public RandomWithWeight(int[] value, int[] weight){
-        this.value = value;
-        this.weight = weight;
+        if (value.length == weight.length) {
+            this.value = value;
+            this.weight = weight;
+        } else {
+            Exception exception = new Exception("problem");
+        }
     }
 
     public int randomWithWeight(){
-        int size = value.length;
-        int choice = (int)(Math.random()*size);
-        return value[choice];
+        int choice = (int) Math.round(Math.random()*sum());
+        return value[selectTheInterval(choice)];
+    }
+
+    private int sum(){
+        int sum = 0;
+        for (int item: weight) {
+            sum += item;
+        }
+        System.out.println("SUM = " + sum);
+        return sum;
+    }
+
+    private int selectTheInterval(int random){
+        int sum = 0;
+        for (int i = 0; i < weight.length ; i++) {
+            sum += weight[i];
+            if(sum >= random) {
+                System.out.println("ran = " + random + " sum = " + sum + " i = " + (i+1));
+                return i;
+            }
+        }
+        return 0;
     }
 }
